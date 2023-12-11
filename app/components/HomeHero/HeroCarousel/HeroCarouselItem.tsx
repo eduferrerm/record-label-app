16d10c047c126a14e42ecb797971441a59cbfa13
eduferrerm <ekferrerm@gmail.com>
2023-12-11 17:48:42 +0100

    Fix error UMD, wrote out relative paths

--- app/components/HomeHero/HeroCarousel/HeroCarouselItem.tsx
+++ app/components/HomeHero/HeroCarousel/HeroCarouselItem.tsx
@@ -1,5 +1,5 @@
-import MDSMetaData from "@/app/components/DesignSystem/MDSMetaData";
-import MDSText from "@/app/components/DesignSystem/MDSText";
+import MDSMetaData from "../../DesignSystem/MDSMetaData";
+import MDSText from "../../DesignSystem/MDSText";
 import { useRef, useState } from "react";
 
 const styles = {
--- app/components/HomeHero/HomeHero.tsx
+++ app/components/HomeHero/HomeHero.tsx
@@ -1,6 +1,6 @@
-import { client } from "@/sanity/client";
+import { client } from "../../../sanity/client";
 
-import HeroCarousel from "@/app/components/HomeHero/HeroCarousel/HeroCarousel";
+import HeroCarousel from "../../components/HomeHero/HeroCarousel/HeroCarousel";
 
 export default async function HomeHero() {
   const bands = await client.fetch<BandAndFeaturedSong[]>(`*[_type == "band"]{
